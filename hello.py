9aee7d0bde2244c64becd57e881696112f6ec5a2
Tony Asleson <tasleson@redhat.com>
2017-03-22 15:43:19 -0500

    Dump sys.path
    Signed-off-by: Tony Asleson <tasleson@redhat.com>

--- hello.py
+++ hello.py
@@ -1,11 +1,23 @@
 #!/usr/bin/env python3
 
 import sys
+import os
 
 try:
     import dbus
 except:
     print('missing dbus!')
 
+print('Dumping sys.path entries')
 if __name__ == '__main__':
+    print('library path= (%s)' % sys.path)
+
+    for directory in sys.path:
+        if os.path.isdir(directory):
+            dir_listing = os.listdir(directory)
+            for d in dir_listing:
+                print('%s/%s' % (directory, d))
+        else:
+            print('sys.path entry %s not a directory' % directory)
+
     print('Hello travis-ci! %s' % str(sys.version_info))
