3f926f84fa086bbd093ceea3db4f720c3389fbee
Tony Asleson <tasleson@redhat.com>
2017-03-22 14:51:06 -0500

    Add block listing & debug
    Signed-off-by: Tony Asleson <tasleson@redhat.com>

--- hello.py
+++ hello.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
 
-import dbus
+import sys
+
+try:
+    import dbus
+except:
+    print('missing dbus!')
 
 if __name__ == '__main__':
-    print('Hello travis-ci!')
+    print('Hello travis-ci! %s' % str(sys.version_info))
